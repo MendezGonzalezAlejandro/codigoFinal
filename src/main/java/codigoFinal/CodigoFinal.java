4d81c7a094b89e95c32a13ec4638a258c9b2f4b1
unknown <alejandromendezgonzalez8@gmail.com>
2021-12-06 21:18:46 -0600

    Metodo procesar cuadrupla

--- src/main/java/codigoFinal/CodigoFinal.java
+++ src/main/java/codigoFinal/CodigoFinal.java
@@ -5,14 +5,109 @@
  */
 package codigoFinal;
 
+import java.io.*;
+import java.util.*;
+
 /**
  *
  * @author mende
  */
 public class CodigoFinal {
+   
+    private PrintWriter fichero;
     
-    public static void main(String[] args) {
-        
+    private void escribirLinea(String linea) {
+		fichero.println(linea);
     }
     
+	private void procesarCuadrupla(Cuadrupla cuadrupla)throws IOException {
+		String op1,op2,inst,res;
+		String linea = " ";
+		op1 = cuadrupla.op1;
+		op2 = cuadrupla.op2;
+		inst = cuadrupla.nombre;
+		res = cuadrupla.resul;
+		if(inst.equals("CARGAR_DIRECCION")) {
+			escribirLinea(linea+"MOVE /"+op1+" , /"+res);
+		} else
+		if(inst.equals("CARGAR_VALOR")) {
+			escribirLinea(linea+"MOVE #"+op1+" , /"+res);
+		} else
+		if(inst.equals("SUMAR")) {
+			escribirLinea(linea+"ADD /"+op1+" , /"+op2);
+			escribirLinea(linea+"MOVE .A , /"+res);
+		} else
+		if(inst.equals("RESTAR")) {
+			escribirLinea(linea+"SUB /"+op1+" , /"+op2);
+			escribirLinea(linea+"MOVE .A , /"+res);
+		} else
+		if(inst.equals("MULTIPLICAR")) {
+			escribirLinea(linea+"MUL /"+op1+" , /"+op2);
+			escribirLinea(linea+"MOVE .A , /"+res);
+		} else
+		if(inst.equals("DIVIDIR")) {
+			escribirLinea(linea+"DIV /"+op1+" , /"+op2);
+			escribirLinea(linea+"MOVE .A , /"+res);
+		} else
+		if(inst.equals("OR")) {
+			escribirLinea(linea+"OR /"+op1+" , /"+op2);
+			escribirLinea(linea+"MOVE .A , /"+res);
+		} else
+		if(inst.equals("AND")) {
+			escribirLinea(linea+"AND /"+op1+" , /"+op2);
+			escribirLinea(linea+"MOVE .A , /"+res);
+		} else
+		if(inst.equals("MAYOR")) {
+			escribirLinea(linea+"CMP /"+op2+" , /"+op1);
+			escribirLinea(linea+"BN $5");
+			escribirLinea(linea+"MOVE #0 , /"+res);
+			escribirLinea(linea+"BR $3");
+			escribirLinea(linea+"MOVE #1 , /"+res);
+		} else
+		if(inst.equals("MENOR")) {
+			escribirLinea(linea+"CMP /"+op1+" , /"+op2);
+			escribirLinea(linea+"BN $5");
+			escribirLinea(linea+"MOVE #0 , /"+res);
+			escribirLinea(linea+"BR $3");
+			escribirLinea(linea+"MOVE #1 , /"+res);
+		} else
+		if(inst.equals("IGUAL")) {
+			escribirLinea(linea+"CMP /"+op1+" , /"+op2);
+			escribirLinea(linea+"BZ $5");
+			escribirLinea(linea+"MOVE #0 , /"+res);
+			escribirLinea(linea+"BR $3");
+			escribirLinea(linea+"MOVE #1 , /"+res);
+		} else
+		if(inst.equals("DISTINTO")) {
+			escribirLinea(linea+"CMP /"+op1+" , /"+op2);
+			escribirLinea(linea+"BZ $5");
+			escribirLinea(linea+"MOVE #1 , /"+res);
+			escribirLinea(linea+"BR $3");
+			escribirLinea(linea+"MOVE #0 , /"+res);
+		} else
+		if(inst.equals("ETIQUETA")) {
+			String lin = res+":"+linea;
+			escribirLinea(lin.substring(0,linea.length())+"NOP");
+		} else
+		if(inst.equals("SALTAR_CONDICION")) {
+			escribirLinea(linea+"CMP #0 , /"+op1);
+			escribirLinea(linea+"BZ /"+res);
+		} else
+		if(inst.equals("SALTAR_ETIQUETA")) {
+			escribirLinea(linea+"BR /"+res);
+		} else
+		if(inst.equals("IMPRIMIR_ENTERO")) {
+			escribirLinea(linea+"WRINT /"+op1);
+		} else
+		if(inst.equals("IMPRIMIR_CADENA")) {
+			escribirLinea(linea+"WRSTR /"+op1);
+		} else
+		if(inst.equals("PONER_CADENA")) {
+			String lin = op1+": DATA"+linea;
+			escribirLinea(lin.substring(0,linea.length())+res);
+		} else
+		if(inst.equals("FIN")) {
+			escribirLinea(linea+"HALT");
+		}
+	}
 }
